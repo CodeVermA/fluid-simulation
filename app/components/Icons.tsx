dc67aaf074bc91d1d515958f8b9f93d7a22e1689
CodeVermA <vasuverma1423@gmail.com>
2026-03-24 16:15:26 +0000

    icon error fixed + benchmarking

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -8,14 +8,26 @@ export const BackIcon = ({ className, ...props }: IconProps) => (
     </svg>
 );
 
-export const ClearIcon = () => (
-    <svg className="w-4 h-4 transition-transform duration-300 hover:-rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+export const ClearIcon = ({ className, ...props }: IconProps) => (
+    <svg
+        className={`w-4 h-4 transition-transform duration-300 hover:-rotate-180 ${className || ''}`}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        {...props}
+    >
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
     </svg>
 );
 
-export const HelpIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+export const HelpIcon = ({ className, ...props }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className={`size-6 ${className || ''}`}
+        {...props}
+    >
         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
     </svg>
 );
